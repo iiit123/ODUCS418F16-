83a41115d59cff1fa44314e559c1e78d0bcf43af
vamshi <vamshi.kolanu@gmail.com>
2016-10-14 23:44:18 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	define('DB_SERVER', 'localhost');
    	define('DB_USERNAME', 'root');
    	define('DB_PASSWORD', '');
-   	define('DB_DATABASE', 'web_programming');
+   	define('DB_DATABASE', 'milestone1dump');
    	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 	session_start();
