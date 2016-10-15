0e5d93e995b4fa3a0e5c4c8841b5ba3ed626cc28
vamshi <vamshi.kolanu@gmail.com>
2016-10-14 23:42:31 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	
 	define('DB_SERVER', 'localhost');
    	define('DB_USERNAME', 'root');
-   	define('DB_PASSWORD', 'password');
+   	define('DB_PASSWORD', '');
    	define('DB_DATABASE', 'web_programming');
    	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
